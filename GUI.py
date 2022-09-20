c812728d4e7f79554c07a0d96f61f0eee49c83a9
Rolol0ko <107160374+Rolol0ko@users.noreply.github.com>
2022-09-20 17:09:52 -0400

    Re-formatted with black

--- GUI.py
+++ GUI.py
@@ -6,68 +6,70 @@ import dice
 
 listOfRolls = []
 
-#functions
+# functions
 def getInput():
     global Input1
     string1 = Input1.get()
     Input1.pack_forget()
     label1.pack_forget()
     label3.config(text=f"Rolls: {string1}")
-    
+
     global Input2
     string2 = Input2.get()
     Input2.pack_forget()
     label2.pack_forget()
     label4.config(text=f"Sides: {string2}")
-    
+
     diceButton.pack_forget()
 
-#add a roll to the list and update the average and if it's fair
+
+# add a roll to the list and update the average and if it's fair
 def addRoll():
     global listOfRolls
     global Input3
-    
-    #check if it's a valid roll
+
+    # check if it's a valid roll
     if int(Input3.get()) < int(int(Input1.get()) + 1):
         int1 = Input3.get()
         listOfRolls.append(int(int1))
         label6.config(text=f"Rolls:{listOfRolls}")
-    
+
         string2 = dice.maths(listOfRolls)
         label7.config(text=f"Average:{string2}")
-    
+
         label8.config(text=f"It is {dice.isItFair(string2, Input2.get())}")
     else:
         messagebox.showerror(title="STUPID", message="That's not a valid number")
 
-#create tkinter window
+
+# create tkinter window
 root = Tk()
 root.geometry("300x360")
 root.title("Die Fairness Checker")
 
-#info on the rolls
-label1 = tkinter.Label(root, text = "Number of Rolls")
+# info on the rolls
+label1 = tkinter.Label(root, text="Number of Rolls")
 label1.pack()
 Input1 = ttk.Entry(root)
 Input1.pack()
 
-#info on the die
-label2 = tkinter.Label(root, text = "How many Sides")
+# info on the die
+label2 = tkinter.Label(root, text="How many Sides")
 label2.pack()
 Input2 = ttk.Entry(root)
 Input2.pack()
 
-#sumbit button
+# sumbit button
 diceButton = ttk.Button(root, text="Submit", width=20, command=getInput)
 diceButton.pack(pady=30)
 
-#confirmation labels (has the # of rolls and the # of sides in a label)
+# confirmation labels (has the # of rolls and the # of sides in a label)
 label3 = tkinter.Label(root)
 label3.pack()
 label4 = tkinter.Label(root)
 label4.pack()
 
-#roll input
+# roll input
 label5 = tkinter.Label(root, text="Roll:")
 label5.pack()
 Input3 = ttk.Entry(root)
@@ -83,5 +85,5 @@ label7.pack()
 label8 = tkinter.Label(root)
 label8.pack()
 
-#run the window
+# run the window
 root.mainloop()
--- dice.py
+++ dice.py
@@ -1,4 +1,4 @@
-#define variables
+# define variables
 rolls = []
 numOfRolls = int
 num = int
@@ -6,30 +6,29 @@ count = int
 sumOfRolls = int
 average = 10
 
+
 def isItFair(x, max):
-    #check if the die is near the range of fair
-    if x < int(max)/2+0.6 and x > int(max)/2-0.6:
-        return("Fair")
+    # check if the die is near the range of fair
+    if x < int(max) / 2 + 0.6 and x > int(max) / 2 - 0.6:
+        return "Fair"
     else:
-        return("Unfair")
+        return "Unfair"
+
 
 def roll(numOfRolls, max):
     cycles = int(0)
     while int(cycles) < int(numOfRolls):
-        num = int(input('-'))
+        num = int(input("-"))
 
         if isinstance(num, int) and int(num) < max and int(num) > 0:
             rolls.append(num)
             cycles += 1
         else:
-            return("Not a valid number")
+            return "Not a valid number"
+
 
 def maths(x):
     count = len(x)
     sumOfRolls = sum(list(x))
-    average = sumOfRolls/count
-    return(average)
-
-#max = input("How many sides on the die? ")
-#print(roll(input("How many rolls? "), (int(max))))
-#print(isItFair(maths(rolls), max))
+    average = sumOfRolls / count
+    return average
